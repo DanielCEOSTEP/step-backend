d2f19c776e61ea4193b1c79e5dce9d4d5beadc28
DanielCEOSTEP <danielkovalenkoph@gmail.com>
2025-02-13 22:07:50 +0100

    Обновление index.js: регистрация и логин

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';  // Этот импорт добавляем
+import cors from 'cors';
 import pkg from 'pg';
 const { Pool } = pkg;
-import jwt from 'jsonwebtoken';  // Этот импорт добавляем
-import bcrypt from 'bcrypt';      // Этот импорт добавляем
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: 'postgresql://postgres:dJXdRcmlKqYgJQUyoyATYEYgmbofLXkn@roundhouse.proxy.rlwy.net:29715/railway',
+  ssl: { rejectUnauthorized: false },
 });
 
-app.use(cors());  // Добавляем использование CORS
+app.use(cors());
 app.use(express.json());
 
 // Создаем таблицу pending_chats, если её нет
@@ -30,8 +28,9 @@ pool.query(`
     UNIQUE (user_id, target_user_id)
   );
 `).then(() => console.log('Table "pending_chats" is ready'))
-  .catch(err => console.error('Error creating table:', err));
+  .catch(err => console.error('Error creating pending_chats table:', err));
 
+// Запуск сервера
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
